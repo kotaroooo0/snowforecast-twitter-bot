c8cc2bcbfd2b23dbcf5fe4f01e78ea566a3cd8bb
kotaroooo0 <k33asby@gmail.com>
2020-04-10 00:01:46 +0900

    コントローラーを追加

---
+++ app/controllers/twitter_controller.go
@@ -0,0 +1,90 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kotaroooo0/snowforecast-twitter-bot/lib/twitter"
+	"github.com/kotaroooo0/snowforecast-twitter-bot/parameters/requests"
+	"github.com/kotaroooo0/snowforecast-twitter-bot/parameters/responses"
+)
+
+type TwitterController struct {
+	engine *gin.Engine
+}
+
+func NewTwitterController(engine *gin.Engine) *TwitterController {
+	return &TwitterController{engine}
+}
+
+func (c *TwitterController) GetCrcToken() {
+	c.engine.Get("/twitter_webhook", c.getCrcToken)
+}
+
+func (c *TwitterController) getCrcToken(ctx *gin.Context) {
+	req := requests.NewGetTwitterWebhookRequest()
+	if err := ctx.Bind(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+	}
+
+	fmt.Print(req)
+
+	res := responses.NewGetTwitterWebhookCrcCheckResponse()
+	res.Token = twitter.CreateCRCToken(req.CrcToken)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (c *TwitterController) PostWebhook() {
+	c.engine.POST("/twitter_webhook/", c.postWebhook)
+}
+
+func (c *TwitterController) postWebhook(ctx *gin.Context) {
+	req := requests.NewPostTwitterWebhookRequest()
+	if err := ctx.Bind(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+	}
+
+	fmt.Print(req)
+	client, err := twitter.CreateTwitterClient()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+	for _, e := range req.DirectMessageEvents {
+		if e.Type != "message_create" {
+			continue
+		}
+		text := "仕事お疲れ様！頑張ったね！！"
+		dmReq := requests.NewPostDirectMessageTestRequest(e.MessageCreate.SenderID, text)
+		fmt.Print(dmReq)
+		if err := twitter.PostDM(client, dmReq); err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+		}
+	}
+	ctx.JSON(http.StatusOK, "")
+}
+
+func (c *TwitterController) PostWebhookTest() {
+	c.engine.POST("/twitter_webhook/test", c.postWebhookTest)
+}
+
+func (c *TwitterController) postWebhookTest(ctx *gin.Context) {
+	req := requests.NewPostTwitterWebhookTestRequest()
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+	}
+
+	fmt.Print(req)
+
+	client, err := twitter.CreateTwitterClient()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	dmReq := requests.NewPostDirectMessageTestRequest(req.ID, req.Message)
+	fmt.Print(dmReq)
+	if err := twitter.PostDM(client, dmReq); err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+	ctx.JSON(http.StatusNoContent, "")
+}
