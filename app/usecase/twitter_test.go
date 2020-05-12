e1ae456d5b6f8c0a5fcf185dedf41ed90948025a
kotaroooo0 <k33asby@gmail.com>
2020-05-13 00:27:01 +0900

    Change redis domain for docker

--- app/usecase/twitter_test.go
+++ app/usecase/twitter_test.go
@@ -32,7 +32,7 @@ type SnowResortRepositoryMock struct {
 
 func testClient() (*redis.Client, error) {
 	client := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: "redis:6379",
 		DB:   1, // 1のDBをテスト用とする
 	})
 	if err := client.Ping().Err(); err != nil {
