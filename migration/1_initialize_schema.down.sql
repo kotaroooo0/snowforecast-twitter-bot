4ca514c7017e70a4c69862d24525875bbbbd9321
kotaroooo0 <k33asby@gmail.com>
2020-07-30 23:45:10 +0900

    add migration files

---
+++ migration/1_initialize_schema.down.sql
@@ -0,0 +1,1 @@
+drop table snow_resorts
---
+++ migration/1_initialize_schema.up.sql
@@ -0,0 +1,7 @@
+create table snow_resorts (
+    id integer auto_increment primary key,
+    name varchar(40),
+    search_key varchar(40),
+    elevation integer,
+    region varchar(40)
+)
