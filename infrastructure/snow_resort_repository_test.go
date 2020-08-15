9348ed24bf9cb1eae07c3d965a7cce2acd9c9ec4
kotaroooo0 <k33asby@gmail.com>
2020-08-16 00:14:00 +0900

    connect docker mysql

--- infrastructure/snow_resort_repository_test.go
+++ infrastructure/snow_resort_repository_test.go
@@ -1,12 +1,32 @@
 package repository
 
 import (
+	"fmt"
 	"testing"
 )
 
 // TODO
+// 今はただの動作確認
+
 func TestFindAll(t *testing.T) {
-	if 1+1 != 2 {
-		t.Error("1 + 1 should be 2")
+	dbConfig := &DBConfig{
+		User:     "root",
+		Password: "password",
+		Addr:     "127.0.0.1",
+		Port:     "3306",
+		DB:       "snowforecast_twitter_bot",
 	}
+	dbClient, err := NewDBClient(dbConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = dbClient.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	r := NewSnowResortRepositoryImpl(dbClient)
+	sr, err := r.FindAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(sr)
 }
